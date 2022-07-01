6a21d9ccd02e7eede5b92cd3c73a02fe52951288
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-06-30 21:20:23 -0300

    Resposta 9: Ela é a Branch principal de quando você cria um repositório, ela serve para salvar todas as demais branchs nela.

--- perguntas.js
+++ perguntas.js
@@ -6,3 +6,4 @@ console.log('5 - Qual o comando para criação de um novo repositório no Git?')
 console.log('6 - O que é o HTTP?');
 console.log('7 - Como funciona o HTTP?');
 console.log('8 - Com o Git Você pode propor mudanças (adicioná-las ao Index) usando um comando. Qual é esse comando?');
+console.log('9 - O que é a Branch master e para que serve?');
