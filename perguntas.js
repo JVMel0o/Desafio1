6868213bcb9b9da004d9fa099d344cbcf0be88f4
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-06-30 21:16:09 -0300

    Resposta 4: O Product Owner é quem paga a funcionalidade do projeto e representa o cliente na equipe de Scrum, ele ouve as opiniões do cliente e decide quais fazem sentido para o projeto, além de ser responsável pelo Product Backlog, a lista de deveres priorizados do que deve ser feito.

--- perguntas.js
+++ perguntas.js
@@ -1,3 +1,4 @@
 console.log('1 - Para que serve o método Scrum?');
 console.log('2 -Como funciona o método Scrum?');
 console.log('3 - O que é Git?');
+console.log('4 - O que é um scrum Product Owner?');
