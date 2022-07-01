b6c85925fcd59e24dbb11416532b4a39c51f7123
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-06-30 21:17:08 -0300

    Resposta 5: git init –bare (para iniciar a pasta que servirá de repositório, esse comando gera um caminho); git remote add nome caminho (para adicionar a pasta como repositório)

--- perguntas.js
+++ perguntas.js
@@ -2,3 +2,4 @@ console.log('1 - Para que serve o método Scrum?');
 console.log('2 -Como funciona o método Scrum?');
 console.log('3 - O que é Git?');
 console.log('4 - O que é um scrum Product Owner?');
+console.log('5 - Qual o comando para criação de um novo repositório no Git?');
