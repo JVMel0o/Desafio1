02d1b273de6eec082da65b7da94e369f9138a9d0
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-06-30 21:24:53 -0300

    Resposta 13: É o status code 201.

--- perguntas.js
+++ perguntas.js
@@ -10,3 +10,4 @@ console.log('9 - O que é a Branch master e para que serve?');
 console.log('10 - Quais são os comandos usados para atualizar um repositório local e fazer merge de um outro branch ao seu branch ativo?');
 console.log('11 - Qual a diferença entre git e github?');
 console.log('12 - Quais os dois verbos http que podemos utilizar para realizar um update? Explique a diferença entre eles.');
+console.log('13 - Qual o status code que pode ser usado na criação de um novo usuário?');
