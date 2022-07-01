905823a6c9bee2533430bc79419268bcc6d5fb07
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-06-30 21:19:29 -0300

    Resposta 8: git add nome do arquivo; Ou para todos os arquivos: git add .

--- perguntas.js
+++ perguntas.js
@@ -5,3 +5,4 @@ console.log('4 - O que é um scrum Product Owner?');
 console.log('5 - Qual o comando para criação de um novo repositório no Git?');
 console.log('6 - O que é o HTTP?');
 console.log('7 - Como funciona o HTTP?');
+console.log('8 - Com o Git Você pode propor mudanças (adicioná-las ao Index) usando um comando. Qual é esse comando?');
