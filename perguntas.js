e189059538ee674221edc56876b31615ad566821
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-06-30 21:17:52 -0300

    Resposta 6: HTTP é um protocolo que estabelece regras de como será a comunicação do cliente/navegador com o servidor web.

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,4 @@ console.log('2 -Como funciona o método Scrum?');
 console.log('3 - O que é Git?');
 console.log('4 - O que é um scrum Product Owner?');
 console.log('5 - Qual o comando para criação de um novo repositório no Git?');
+console.log('6 - O que é o HTTP?');
